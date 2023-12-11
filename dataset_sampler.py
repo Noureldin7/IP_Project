79c615d420cfb61fb961748a394404e46160a288
Noureldin7 <noureldin.salah01@eng-st.cu.edu.eg.com>
2023-12-11 18:35:02 +0200

    57% Accuracy

--- dataset_sampler.py
+++ dataset_sampler.py
@@ -8,13 +8,11 @@ folder_names = ['one', 'two_up', 'three', 'four', 'palm']
 # Directory path for the dataset
 dataset_dir = "./hagrid-classification-512p"
 # Number of samples to extract
-number_of_samples = 10
+number_of_samples = 10000
 
 for folder in folder_names:
     file_names = os.listdir(f'{dataset_dir}/{folder}')
     random_files = random.sample(file_names, number_of_samples)
     os.makedirs(f'./samples/{folder}', exist_ok=True)
     i=1
-    for file in random_files:
-        shutil.copy2(f'./{dataset_dir}/{folder}/{file}', f'./samples/{folder}/{i}{file[-5:]}')
-        i+=1
+    [shutil.copy2(f'./{dataset_dir}/{folder}/{file}', f'./samples/{folder}/{i+1}{file[-5:]}') for i, file in enumerate(random_files)]
