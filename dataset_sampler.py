72154877aae2353d222d5e07ae404e5fc53c598b
Mostafa El-Nashar <mostafaelnashar@outlook.com>
2023-12-12 11:03:13 +0200

    Run and save pkl for model

--- dataset_sampler.py
+++ dataset_sampler.py
@@ -6,11 +6,11 @@ import random
 # set random seed
 random.seed(42)
 # Folder names of classes to be sampled
-folder_names = ['one', 'two_up', 'three', 'four', 'palm']
+folder_names = ["call", "rock", "dislike", "fist", "palm", "peace", "one", "ok", "like"]
 # Directory path for the dataset
-dataset_dir = "./hagrid-classification-512p"
+dataset_dir = "./hagrid-classification-128p-squares/"
 # Number of samples to extract
-number_of_samples = 10000
+number_of_samples = 100
 
 for folder in folder_names:
     file_names = os.listdir(f'{dataset_dir}/{folder}')
